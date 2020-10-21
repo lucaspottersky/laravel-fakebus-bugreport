043eac28b145dd9c2b6e591577452b692623d4ad
Lucas Pottersky <lucas_arrruda@hotmail.com>
2020-10-21 19:52:51 +0000

    bug report

---
+++ app/Jobs/OneJob.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Jobs;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class OneJob implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle()
+    {
+        logger('OneJob running...');
+    }
+}
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,40 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Jobs\FourJob;
+use App\Jobs\OneJob;
+use App\Jobs\ThreeJob;
+use App\Jobs\TwoJob;
+use Illuminate\Support\Facades\Bus;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    // Outputs:
+    // testing.DEBUG: OneJob running...
+    public function testWontRunAfterTwoJob()
     {
-        $this->assertTrue(true);
+        Bus::fake([TwoJob::class]);
+
+        Bus::chain([
+            new OneJob(),
+            new TwoJob(),
+            new ThreeJob(),
+        ])->dispatch();
+    }
+
+    // Outputs:
+    // testing.DEBUG: OneJob running...
+    // testing.DEBUG: TwoJob running...
+    public function testWontRunAfterThreeJob()
+    {
+        Bus::fake([ThreeJob::class]);
+
+        Bus::chain([
+            new OneJob(),
+            new TwoJob(),
+            new ThreeJob(),
+            new FourJob(),
+        ])->dispatch();
     }
 }
